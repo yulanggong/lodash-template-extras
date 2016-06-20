dd11e707321c3f9a9afaa3fba7b262ca0d8a5664
brinsh <(none)>
2016-06-20 10:35:22 +0800

    add src

---
+++ src/underscore-template-extras.js
@@ -0,0 +1,123 @@
+;(function () {
+	var templates = {};
+	var templateHelpers = {};
+
+
+	_.templateEx = function(tplName, data){
+		return templates[tplName].compile(data);
+	};
+
+
+	var addTemplate = function(name, text, _templates){
+		var compile;
+		var parent;
+		var alias;
+		var blocks = {};
+		var overrides = {};
+		var prepends = {};
+		var appends = {};
+		var markedText;
+		var compileText;
+
+		markedText = text.replace(/<%\s*@alias\(['"]([^'"]+)['"]\)\s*%>/g, function(all, name){
+			alias = name;
+			return '';
+		});
+
+		markedText = markedText.replace(/<%\s*@extends\(['"]([^'"]+)['"]\)\s*%>/g, function(all, name){
+			parent = templates[name];
+
+			if(!parent){
+				addTemplate(name, _templates[name]);
+				parent = templates[name];
+			}
+
+			return '';
+		});
+
+		markedText = markedText.replace(/<block\s+name=['"]?([^'">\s]+)['"]?\s*\/>/g, function(all, name){
+			return '<block '+ name +'>';
+		});
+
+		markedText = markedText.replace(/<block\s+name=['"]?([^'">\s]+)['"]?\s*>([\s\S]*?)<\/block>/g, function(all, name, block){
+			blocks[name] = block;
+			return '<block '+ name +'>';
+		});
+
+		//解析 override
+		markedText = markedText.replace(/<override\s+name=['"]?([^'">\s]+)['"]?\s*>([\s\S]*?)<\/override>/g, function(all, name, override){
+			overrides[name] = override;
+			return '';
+		});
+
+		markedText = markedText.replace(/<prepend\s+name=['"]?([^'">\s]+)['"]?\s*>([\s\S]*?)<\/prepend>/g, function(all, name, prepend){
+			prepends[name] = prepend;
+			return '';
+		});
+
+		markedText = markedText.replace(/<append\s+name=['"]?([^'">\s]+)['"]?\s*>([\s\S]*?)<\/append>/g, function(all, name, append){
+			appends[name] = append;
+			return '';
+		});
+
+		if(parent) {
+			markedText = parent.markedText.replace(/<block\s([^>]+)>/g, function(all, name){
+				return (prepends[name]||'')
+					+ (overrides[name] != null ? overrides[name] :
+						parent&&parent.blocks[name] != null ? parent.blocks[name] : '')
+					+ (appends[name]||'');
+			});
+		}
+
+		compileText = markedText.replace(/<block\s([^>]+)>/g, function(all, name){
+			return blocks[name]||'';
+		});
+
+
+		compileText = compileText.replace(/(<%|${)\s*@([^\(]+)\(/g, function(all, tag, name){
+			return (tag == '${'? tag : (tag + '=')) + '_.templateEx.helper(\'' + name + '\',';
+		});
+
+		compile = _.template(compileText, {sourceURL: 'templates/'+name+'.html'});
+
+		templates[name] = {
+			text: text,
+			markedText: markedText,
+			blocks: blocks,
+			compile: compile,
+			name: name
+		};
+
+		if (alias) {
+			templates[name].alias = alias;
+			templates[alias] = templates[name];
+		}
+	};
+
+	_.templateEx.add = function(name, text){
+		if(arguments.length == 2){
+			addTemplate(name, text);
+		} else {
+			_.each(name, function(text, name, templates){
+				addTemplate(name, text, templates);
+			})
+		}
+	};
+
+	_.templateEx.addHelper = function(name, fn){
+		templateHelpers[name] = fn;
+	};
+
+	_.templateEx.helper = function(name/*, ...rest*/){
+		var helper = templateHelpers[name];
+		var rest = [].slice.call(arguments, 0, arguments.length);
+		rest.shift();
+		if(!helper){
+			helper = templates[name].compile;
+			if(!rest.length) rest.push({});
+		}
+		return helper ? helper.apply(_, rest) : '';
+	};
+
+	_.templateEx.addHelper('include', _.templateEx);
+})();
